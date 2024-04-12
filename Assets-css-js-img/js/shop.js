6a1a1d04a840deefcc0adb229f2f1409c0d6d185
Jenny Johnsby <152171455+JennyJohnsby@users.noreply.github.com>
2024-04-13 00:38:19 +0200

    start of js

--- Assets-css-js-img/js/shop.js
+++ Assets-css-js-img/js/shop.js
@@ -1,1 +1,15 @@
-console.log("here")
+fetch("https://api.noroff.dev/api/v1/rainy-days")
+.then(res => {
+    return res.json()
+})
+.then(data => {
+    const allProducts = data; // Array av jakker
+    const firstJacket = allProducts[0];
+    const thirdJacket = allProducts[2];
+    console.log(firstJacket.title +" "+ firstJacket.id)
+const title = document.createElement("h3")
+title.innerText = thirdJacket.title
+const section = document.querySelector("#jacket-title")
+console.log (section)
+section.append(title)
+})
